aad0d4a92d620054fd569c7c483ef311555f46d5
Simeon Franklin <sfranklin@twitter.com>
2019-02-20 20:13:07 -0800

    Expanding args and kwargs example

--- intermediate/args_and_kwargs.py
+++ intermediate/args_and_kwargs.py
@@ -47,12 +47,81 @@ which will hold any *positional* arguments passed to the function. It works like
 
 As you can see args is a tuple of all the values passed into the function.
 
+Important! The name "args" is arbitrary. You could call it something
+else - but its a tradition to call this parameter args.
+
 Can you make add take an arbitrary number of arguments?
 
 >>> add(1, 2, 3)
 6
 >>> add(1, 2, 3, 4)
 10
+
+# What about names?
+
+It's also important to note that `*args` only helps you with
+positional arguments. You may not realize it but you can also pass
+arguments by name! For instance make a subtract function with
+parameter names x and y. It should work like this:
+
+>>> subtract(5, 2)
+3
+
+That's pretty normal - the `5` above is the first positional argument
+and it went to the first parameter in your function definition, the
+`2` is the second positional argument and went to the second
+parameter. But what if I called it like this?
+
+>>> subtract(y=2, x=5)
+3
+
+Huh! You can pass arguments by name in which case position doesn't
+matter! So how about our old friend variadic? Can we pass it arguments by name?
+
+>>> variadic(x=1)
+Traceback (most recent call last):
+...
+TypeError: variadic() got an unexpected keyword argument 'x'
+
+It turns out *args only helps us with positional arguments. To catch
+arguments that are passed by name you need *args starry friend
+**kwargs! Just like *args is a catchall for unaccounted for positional
+args, **kwargs is a catchall for unaccounted for named arguments.
+
+>>> def variadic2(**kwargs):
+...     return kwargs
+>>> variadic2(x=1, y=2)
+{'y': 2, 'x': 1}
+
+So consider how to write functions that can be called like this:
+
+>>> color(255)
+(255, 0, 0)
+>>> color(red=255)
+(255, 0, 0)
+>>> color(255, 10, 1, alpha=1.4) # you don't have to do anything with alpha
+(255, 10, 1)
+>>> color(255, 10, 1, extra="foo") # or extra. Just don't crash!
+(255, 10, 1)
+
+Why would you ever want to use **kwargs? Hint: checkout the help for `dict`.
+
+# Calling with * and **
+
+Calling a function with * and ** is just like defining, but the inverse.
+
+In a def line * and ** pack incoming values (or name/value pairs) into
+a container. When calling a function * unpacks a positional container
+into positional args and ** unpacks a dict into named args.
+
+Assuming you named your function with red, green and blue arguments you might have data in a list:
+
+>>> color_list = [255, 200, 100]
+>>> color(*color_list)
+>>> color_dict = {'red': 255, 'green': 200, 'blue': 100}
+>>> color(**color_dict)
+
+Does this make sense?
 
 # Running the exercises
 
